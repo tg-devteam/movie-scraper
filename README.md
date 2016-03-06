16661648c97f0802c00e3c36800b191d0ab0080e
Zach Toben <zach.toben@gmail.com>
2016-03-06 15:27:37 -0600

    Add sass compiling to server start

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
+# Requirements
+* Install node.js version 5.4.0 or greater
+* Install bower using npm
+
 # Get started
 * npm install
 * bower install
 * npm start
 
-
+# Run the app
 Point browser to localhost:3000
+
+# Notes
+* Styling is done with sass, if you modify the style.css file it will be over-written
+* This app uses the MEAN stack
+ * MongoDb
+ * Express
+ * Angular
+ * Node
--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var express         = require('express');
 var mongoose        = require('mongoose');
 var port            = process.env.PORT || 3000;
 var morgan          = require('morgan');
-var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
+var sass            = require('node-sass');
+var fs              = require('fs');
 var app             = express();
 
 // Express Configuration
@@ -17,15 +18,27 @@ var app             = express();
 app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
 app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
 app.use(morgan('dev'));                                         // log with Morgan
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
-app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
-app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
 app.use(methodOverride());
 
 // Routes
 // ------------------------------------------------------
 // require('./app/routes.js')(app);
+
+// Compile the sass
+// ------------------------------------------------------
+sass.render({
+    file: __dirname + '/public/style.scss',
+    outFile: __dirname + '/public/style.css',
+    outputStyle: 'compressed'
+}, function(error, result) {
+    if(!error){
+        fs.writeFile(__dirname + '/public/style.css', result.css, function(err){
+            if(!err){
+                console.log('Sass has been saved.')
+            }
+        });
+    }
+});
 
 // Listen
 // -------------------------------------------------------
